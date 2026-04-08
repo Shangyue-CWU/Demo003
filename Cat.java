4ff48cd83d211524b6f8323fa5e18aa87ca1f845
Shangyue-CWU <144855209+Shangyue-CWU@users.noreply.github.com>
2026-04-08 16:13:57 -0700

    Update Cat.java
    Add hello CS380

--- Cat.java
+++ Cat.java
@@ -3,6 +3,7 @@ public class TestSuite {
 	public static void main(String[] args) {
         System.out.println("Hello, Cat!");
         System.out.println("Hello, Duck!");
+		System.out.println("Hello, CS380!");
 
     }
 
